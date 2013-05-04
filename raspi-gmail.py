3fd234b9591b80c84f7a224df45d5e93693ff070
patback66 <maskoken@gmail.com>
2013-05-04 16:50:44 -0700

    Added email notification for Raspberry Pi with LEDs.Requires login.

---
+++ raspi-gmail.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO, feedparser, time
+import getpass
+DEBUG = 1
+
+#USERNAME = "maskoken"     # just the part before the @ sign, add yours here
+USERNAME = raw_input("Username (ex. username@gmail.com): ")
+PASSWORD = getpass.getpass(prompt="Password: ")
+
+NEWMAIL_OFFSET = 0        # my unread messages never goes to zero, yours might
+MAIL_CHECK_FREQ = 30      # check mail every 60 seconds
+
+GPIO.setmode(GPIO.BCM)
+GREEN_LED = 18
+RED_LED = 23
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+last = -1
+while True:
+
+        newmails = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom")["feed"]["fullcount"])
+
+        if DEBUG:
+		if (last!=newmails):
+	                print "You have", newmails, "new emails!"
+
+        if newmails > NEWMAIL_OFFSET:
+                GPIO.output(GREEN_LED, True)
+                GPIO.output(RED_LED, False)
+        else:
+                GPIO.output(GREEN_LED, False)
+                GPIO.output(RED_LED, True)
+	last = newmails
+        time.sleep(MAIL_CHECK_FREQ)
+
